a50eda0f712a7bf56a3f31dcc3d62b2b0148957f
Kajetan7 <krmroske@gmail.com>
2023-03-24 16:35:46 +0100

    First issue

--- lotto_simulator.py
+++ lotto_simulator.py
@@ -17,6 +17,9 @@ for i in range(6):
             if user_selection in list_of_user_numbers:
                 print("You have already used that number!")
                 continue
+            if user_selection > 49 or user_selection < 1:
+                print("You have to pick number in between 1 and 49!")
+                continue
             if 50 > user_selection > 0:
                 list_of_user_numbers.append(user_selection)
                 break
@@ -26,9 +29,25 @@ for i in range(6):
 list_of_user_numbers.sort()
 print(f"\n Here are your numbers: \n {list_of_user_numbers} \n")
 
-numbers_selected_by_machine = [random.randint(1, 49) for i in range(6)]
-numbers_selected_by_machine.sort()
+
+list_of_numbers_selected_by_machine = []
+for i in range(6):
+    while True:
+        machine_choice = random.randint(1, 49)
+        if machine_choice not in list_of_numbers_selected_by_machine:
+            list_of_numbers_selected_by_machine.append(machine_choice)
+            break
+        continue
+
+list_of_numbers_selected_by_machine.sort()
 
 print(f'''... and here are the numbers picked by the machine!
-{numbers_selected_by_machine}
+{list_of_numbers_selected_by_machine}
 ''')
+
+list_of_numbers_won = set(list_of_user_numbers) & set(list_of_numbers_selected_by_machine)
+number_of_numbers_won = len(list_of_numbers_won)
+if number_of_numbers_won > 2:
+    print(f'Congratulations! You got {number_of_numbers_won} numbers that match!')
+else:
+    print(f'We are really sorry, you got {number_of_numbers_won} numbers that match..')
